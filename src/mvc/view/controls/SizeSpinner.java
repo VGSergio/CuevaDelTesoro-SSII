9c1c3b4ce4dbb4dd503ad2630b16257fd8caacd6
VGSergio <svegasise@gmail.com>
2024-11-13 23:39:25 +0100

    sizeSpinner texfield disabled

--- src/mvc/view/controls/SizeSpinner.java
+++ src/mvc/view/controls/SizeSpinner.java
@@ -3,18 +3,24 @@ package mvc.view.controls;
 import mvc.controller.Controller;
 
 import javax.swing.*;
-
 import java.awt.*;
 
 import static mvc.model.Global.*;
 
 public class SizeSpinner extends JPanel {
 
+    private final JSpinner spinner;
+
     public SizeSpinner(Controller controller) {
         // Initialize components
         JLabel nLabel = new JLabel("N");
-        JSpinner spinner = new JSpinner(new SpinnerNumberModel(MIN_MAZE_SIDE, MIN_MAZE_SIDE, MAX_MAZE_SIDE, 1));
+        nLabel.setToolTipText("Set the maze side size");
+
+        spinner = new JSpinner(new SpinnerNumberModel(MIN_MAZE_SIDE, MIN_MAZE_SIDE, MAX_MAZE_SIDE, 1));
+        spinner.setToolTipText("Select the size of one side of the maze");
+
         spinner.addChangeListener(e -> controller.notify(MAZE_SIDE_CHANGED, spinner.getValue()));
+        ((JSpinner.DefaultEditor) spinner.getEditor()).getTextField().setEditable(false);
 
         // Configure layout
         setLayout(new BorderLayout());
